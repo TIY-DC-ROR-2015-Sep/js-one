c3860e11f66bc2f40a66ee0055aea46d8f50c481
James Dabbs <jamesdabbs@gmail.com>
2015-11-11 09:43:07 -0500

    Ruby form

--- app/controllers/js_demo_controller.rb
+++ app/controllers/js_demo_controller.rb
@@ -2,4 +2,9 @@ class JsDemoController < ApplicationController
   def counter
     # Just render counter.html.erb as usual
   end
+
+  def receive_message
+    flash[:notice] = params[:thing_to_echo]
+    redirect_to :back
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  post "/message" => "js_demo#receive_message"
   root "js_demo#counter"
 end
