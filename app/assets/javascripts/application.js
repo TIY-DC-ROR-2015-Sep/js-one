cc20d8e8e9f19ccce2bfcbec823325c9a3622af6
James Dabbs <jamesdabbs@gmail.com>
2015-11-11 09:54:23 -0500

    Remove js from inline in view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,40 @@
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
+
+// window.location = "http://google.com"
+// window.onload = function() { alert("loaded"); }
+
+// alert("Before attaching load");
+window.onload = function() {
+  // alert("Document loaded");
+  var count = 0;
+
+  var incButtons = document.querySelectorAll(".increment-counter");
+  var displayNode = document.querySelector("span.counter-display");
+
+  // incButtons.each do |incButton|
+  for (var i = 0; i < incButtons.length; i ++) {
+    var incButton = incButtons[i];
+    incButton.addEventListener("click", function(e) {
+      // This stops the link from actually making a GET request
+      e.preventDefault();
+
+      // alert("Button was clicked");
+      if (count < 10) {
+        count += 1;
+        displayNode.innerText = count;
+      } else {
+        displayNode.innerHTML = '<img src="https://i.imgflip.com/txo8v.jpg">';
+      }
+
+      // Here's one way to grab the node that was clicked ...
+      var clicked = e.target;
+      // ... and do something with it
+      var newNode = document.createElement("span");
+      newNode.innerText = "·";
+      clicked.appendChild(newNode);
+    });
+  }
+}
+// alert("Down here");
